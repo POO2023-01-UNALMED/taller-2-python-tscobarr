c8e4812f694fd03da08f1ada9db4da7edb0304ba
tscobarr <124937392+tscobarr@users.noreply.github.com>
2023-02-19 21:09:28 -0500

    :alien:

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ class Asiento:
 
 class Auto:
     cantidadCreados = 0
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        self.cantidadCreados = cantidadCreados
 
     def cantidadAsientos(self):
         asientos = 0;
