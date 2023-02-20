ac3fca37cba57051b5624a8bb389f1a3f808fb34
tscobarr <124937392+tscobarr@users.noreply.github.com>
2023-02-19 20:53:09 -0500

    :airplane:

---
+++ main.py
@@ -0,0 +1,54 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if color == 'rojo' or color == 'verde' or color == 'amarillo' or color == 'negro' or color == 'blanco':
+            self.color = color
+
+class Auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+
+    def cantidadAsientos(self):
+        asientos = 0;
+        for i in range (self.asientos):
+            if i != None:
+                asientos += 1
+        return asientos
+    
+    def verificarIntegridad(self):
+        if self.motor.registro != self.registro:
+            return 'Las piezas no son originales'
+
+        for j in range (self.asientos):
+            if j != None:
+                if j.registro != self.registro:
+                    return 'Las piezas no son originales'
+
+
+        return 'Auto original'
+
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if tipo == 'electrico' or 'gasolina':
+            self.tipo = tipo
+
+
